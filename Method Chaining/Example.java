608f7a49e9f3ed991c1087e39cf4cb053c92d672
chethanbhat7 <chethanbhat777@gmail.com>
2026-01-31 14:26:27 +0530

    Added Method Chaining

---
+++ Method Chaining/Example.java
@@ -0,0 +1,66 @@
+class Example {
+    public static void Find(int n)
+    {   
+        Check1(n);
+        Check2(n);
+        Check3(n);
+        Check4(n);
+        Check5(n);
+        Check6(n);
+        Check7(n);
+        Check8(n);
+        Check9(n);
+    }
+
+    public static void Check1(int n)
+    {
+        if(n==1)
+        System.out.println("Value is 1");
+    }
+
+    public static void Check2(int n)
+    {
+        if(n==2)
+        System.out.println("Value is 2");
+    }
+
+    public static void Check3(int n)
+    {
+        if(n==3)
+        System.out.println("Value is 3");
+    }
+    public static void Check4(int n)
+    {
+        if(n==4)
+        System.out.println("Value is 4");
+    }
+    public static void Check5(int n)
+    {
+        if(n==5)
+        System.out.println("Value is 5");
+    }
+    public static void Check6(int n)
+    {
+        if(n==6)
+        System.out.println("Value is 6");
+    }
+    public static void Check7(int n)
+    {
+        if(n==7)
+        System.out.println("Value is 7");
+    }
+    public static void Check8(int n)
+    {
+        if(n==8)
+        System.out.println("Value is 8");
+    }
+    public static void Check9(int n)
+    {
+        if(n==9)
+        System.out.println("Value is 9");
+    }
+
+    public static void main(String[] args) {
+        Find(1);
+    }
+}
