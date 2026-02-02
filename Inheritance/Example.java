d8b89b63d048d050b8a9a50da2e8a1e2a0461c1f
chethanbhat7 <chethanbhat777@gmail.com>
2026-02-02 14:51:49 +0530

    Added Inheritance

---
+++ Inheritance/Example.java
@@ -0,0 +1,29 @@
+class ParentClass{
+    static String name;
+    static int age;
+    static String address;
+    
+    static{
+    System.out.println("HELLO PARENT");
+    }
+}
+
+class ChildClass extends ParentClass{
+    static String hobby;
+    static{
+    System.out.println("HELLO CHILD");
+    }
+}
+
+
+class Example{
+    public static void main(String[] args) {
+        ChildClass child=new ChildClass();
+        child.name="Ashith";
+        child.age=20;
+        child.address="VCET";
+        child.hobby="Body Building";
+        System.out.println(child.hobby);
+        ChildClass newchild= new ChildClass();
+    }
+}
---
+++ Inheritance/ExamplewithNonStatic.java
@@ -0,0 +1,21 @@
+class ParentClass{
+     String name;
+     int age;
+     String address;
+}
+
+class ChildClass extends ParentClass{
+     String hobby;
+}
+
+
+class ExamplewithNonStatic{
+    public static void main(String[] args) {
+        ChildClass child=new ChildClass();
+        child.name="Ashith";
+        child.age=20;
+        child.address="VCET";
+        child.hobby="Body Building";
+        System.out.println(child.hobby);
+    }
+}
