480b373df898a6584cad601e5166c6cf4324f955
chethanbhat7 <chethanbhat777@gmail.com>
2026-02-04 12:47:55 +0530

    Added Overiding of Equals Fn

---
+++ String Class/OverideEquals.java
@@ -0,0 +1,30 @@
+class AshithGF{
+    String name;
+    int age;
+
+    public boolean equals(AshithGF gf)
+    {
+    if (this.age==gf.age && this.name==gf.name){
+        return true;
+    }   
+    return false;
+    }
+
+    public AshithGF(String name,int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+}
+
+
+class OverideEquals {
+     
+    public static void main(String[] args) {
+        AshithGF Fathima = new AshithGF("Fathima",20);
+        AshithGF Gathima = new AshithGF("Fathima",20);
+        System.out.println(Fathima.equals(Gathima));
+    }
+
+
+}
