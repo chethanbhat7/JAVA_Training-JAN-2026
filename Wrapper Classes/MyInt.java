132514e356428b64034378fb7c0508b167bb139f
chethanbhat7 <chethanbhat777@gmail.com>
2026-02-06 10:12:25 +0530

    Added Custom Long type

--- Wrapper Classes/MyInt.java
+++ Wrapper Classes/MyInt.java
@@ -2,6 +2,9 @@ class MyInt {
     public static void main(String[] args) {
         NewInt a=new NewInt(10);
         System.out.println(a);
+
+        NewLong b=new NewLong(900000000);
+        System.out.println(b);
     }
 }
 
@@ -10,6 +13,21 @@ class NewInt
     int data=10;
 
     public NewInt(int data) {
+        this.data=data;
+    }
+
+    @Override
+    public String toString() {
+        return ""+data;
+    }
+
+    
+    
+}
+
+class NewLong{
+    long data;
+    public NewLong(long data) {
         this.data=data;
     }
 
